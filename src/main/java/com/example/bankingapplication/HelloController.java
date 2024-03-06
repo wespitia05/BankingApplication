ac74bc49596919955e9233d99836ce6b56a5f589
william <espiw@farmingdale.edu>
2024-03-05 19:04:47 -0500

    created initialize method

---
+++ src/main/java/com/example/bankingapplication/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.bankingapplication;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    public void initialize () {
+        System.out.println ("Initialize called");
+    }
+}
