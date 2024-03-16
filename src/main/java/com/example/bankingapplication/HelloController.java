c62a1d4728ec1f8efa40004feba425448ea0d6f4
william <espiw@farmingdale.edu>
2024-03-16 01:31:38 -0400

    implemented basic layout for the log in screen with name of the bank, username and password text fields, and log in and create account buttons.

--- src/main/java/com/example/bankingapplication/HelloController.java
+++ src/main/java/com/example/bankingapplication/HelloController.java
@@ -1,10 +1,19 @@
 package com.example.bankingapplication;
 
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class HelloController {
     public void initialize () {
         System.out.println ("Initialize called");
+
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.example.bankingapplication {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires com.healthmarketscience.jackcess;
 
     opens com.example.bankingapplication to javafx.fxml;
     exports com.example.bankingapplication;
